5f8e822237d46b34af0caf26fe01b137d2ef3287
Yioelmn <SSDD@POAPCA02LC0514.duoc.acad>
2025-11-13 16:07:02 -0300

    Commit 1 creacion de API

---
+++ src/test/java/com/example/ExoticWorld/ExoticWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExoticWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoticWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
